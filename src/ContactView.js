d702d3ed60eddefb62c14dcb4f6a5ffd8110dc45
Ewa Klebba <klebba.ewa@gmail.com>
2018-05-04 22:01:49 +0200

    Handing input change and syncing it with state

--- src/ContactView.js
+++ src/ContactView.js
@@ -2,24 +2,59 @@ import React, { Component, Fragment } from 'react';
 
 class ContactView extends Component {
 
+  state = {
+    name: '',
+    phone: '',
+    email: '',
+    groups: '',
+
+    contacts: []
+  }
+
   handleSubmit = event => {
-    event.preventDefault()
+    event.preventDefault();
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
     return (
       <Fragment>
-        <form onSubmit={this.handleSubmit}>
-          <input name="name" placeholder="Name" />
-          <br />
-          <input name="phone" placeholder="Phone" />
-          <br />
-          <input name="email" placeholder="E-mail" />
-          <br />
-          <input name="groups" placeholder="Groups" />
-          <br />
-          <button>Add</button>
-        </form>
+        <div class="add-new">
+          <h2 class="App-title ">Add new contact</h2>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              name = "name"
+              value = {this.state.name}
+              onChange = {this.handleChange}
+              placeholder = "Name" />
+            <br />
+            <input
+              name="phone"
+              value = {this.state.phone}
+              onChange = {this.handleChange}
+              placeholder="Phone" />
+            <br />
+            <input
+              name="email"
+              value = {this.state.email}
+              onChange = {this.handleChange}
+              placeholder="E-mail" />
+            <br />
+            <input
+              name="groups"
+              value = {this.state.groups}
+              onChange = {this.handleChange}
+              placeholder="Groups" />
+            <br />
+            <button>Add</button>
+          </form>
+        </div>
+
 
         <ul>
 
