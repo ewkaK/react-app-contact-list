44aa7dd7b22290a416972c38e88a12d485a25cf6
Ewa Klebba <klebba.ewa@gmail.com>
2018-05-04 21:28:25 +0200

    Form and list for contacts + handleSubmit event.preventDefault()

--- src/ContactView.js
+++ src/ContactView.js
@@ -1,15 +1,29 @@
 import React, { Component, Fragment } from 'react';
 
 class ContactView extends Component {
+
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
       <Fragment>
-        <form>
-          <input name="name" placeholder="Name"/>
+        <form onSubmit={this.handleSubmit}>
+          <input name="name" placeholder="Name" />
+          <br />
           <input name="phone" placeholder="Phone" />
+          <br />
           <input name="email" placeholder="E-mail" />
+          <br />
           <input name="groups" placeholder="Groups" />
+          <br />
+          <button>Add</button>
         </form>
+
+        <ul>
+
+        </ul>
       </Fragment>
     )
   }
